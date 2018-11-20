1eb2dfb70d61db6164305e3f0835f90b72fad943
sergeyerr <37268657+sergeyerr@users.noreply.github.com>
2018-11-20 21:42:40 +0500

    working backchange

--- Окошечко с нуля/Graphics.cpp
+++ Окошечко с нуля/Graphics.cpp
@@ -40,6 +40,8 @@ void GridAndCirclesPainting(HWND handleWindow) {
 void BackGroundPaint(HWND handleWindow, WPARAM wParam) {
 	HBRUSH	brush;
 	RECT windowRectangle;
+	
+	HDC handleDC = GetDC(handleWindow);
 	int dR = 0, dG = 0, dB = 0;
 	WaitForSingleObject(BackColorMutex, INFINITE);
 	if (options.TargetColor.r > options.NowColor.r) dR = 1;
@@ -53,30 +55,11 @@ void BackGroundPaint(HWND handleWindow, WPARAM wParam) {
 	options.NowColor.g += dG;
 	options.NowColor.b += dB;
 	brush = CreateSolidBrush(RGB(options.NowColor.r, options.NowColor.g, options.NowColor.b));
-	SelectObject((HDC)wParam, brush);
+	SelectObject(handleDC, brush);
 	GetClientRect(handleWindow, &windowRectangle);
 	options.WindowSize.first = windowRectangle.right - windowRectangle.left;
 	options.WindowSize.second = windowRectangle.bottom - windowRectangle.top;
-	Rectangle((HDC)wParam, windowRectangle.left, windowRectangle.top, windowRectangle.right, windowRectangle.bottom);
+	Rectangle(handleDC, windowRectangle.left, windowRectangle.top, windowRectangle.right, windowRectangle.bottom);
 	DeleteObject(brush);
-
-	PAINTSTRUCT paintStruct;
-	HDC handleDC = BeginPaint(handleWindow, &paintStruct);
-	SelectObject(handleDC, options.LinePen);
-	for (int i = 0; i < options.n + 1/*windowRectangle.bottom / options.CellSize + 1*/; i++) {
-		MoveToEx(handleDC, 0, i  * options.CellSize, NULL);
-		LineTo(handleDC, options.CellSize * options.m /*windowRectangle.right*/, i  * options.CellSize);
-	}
-	for (int i = 0; i < options.m + 1/*windowRectangle.right / options.CellSize + 1*/; i++) {
-		MoveToEx(handleDC, i  * options.CellSize, 0, NULL);
-		LineTo(handleDC, i  * options.CellSize, options.CellSize * options.n);
-	}
-	for (int i = 0; i < options.n; i++) {
-		for (int j = 0; j < options.m; j++) {
-			if ((*PlacedPictures)[i][j] == 255) continue;
-			PaintCircle(handleDC, j * options.CellSize, i * options.CellSize, (*PicturesBitmaps)[(*PlacedPictures)[i][j]]);
-		}
-	}
-	EndPaint(handleWindow, &paintStruct);
 	DeleteObject(handleDC);
 }
